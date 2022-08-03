c2a3fb7babd6adc052d86d438c5d236d201b37d0
LucasRomanato0 <20.00313-7@maua.br>
2022-08-03 16:05:24 -0300

    atualização

--- maua_reservation/lib/modules/login/pages/login_page.dart
+++ maua_reservation/lib/modules/login/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,23 +9,74 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     String imagePath = 'assets/images/top_login.png';
     return Scaffold(
-      backgroundColor: Colors.white,
+      // backgroundColor: Colors.black,
       body: Center(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 96),
-              child: Container(
-                height: 315,
-                width: 450,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(100),
-                      topRight: Radius.circular(100)),
-                  image: DecorationImage(image: AssetImage(imagePath)),
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                // foto do topo
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage(imagePath)),
+                  ),
                 ),
-              ),
+              ],
             ),
+            // corpo (principal)
+            // Stack(
+            //   alignment: Alignment.topCenter,
+            //   children: [
+            //     // parte branca
+            //     Container(
+            //       height: 500,
+            //       width: 450,
+            //       decoration: const BoxDecoration(
+            //         color: Colors.white,
+            //         borderRadius: BorderRadius.all(Radius.circular(50)),
+            //       ),
+            //     ),
+            //     Padding(
+            //       padding: const EdgeInsets.only(top: 32),
+            //       child: Container(
+            //         width: 400,
+            //         height: 200,
+            //         decoration: const BoxDecoration(
+            //           color: Color.fromARGB(70, 167, 167, 167),
+            //           borderRadius: BorderRadius.all(Radius.circular(50)),
+            //         ),
+            //         child: Padding(
+            //           padding: const EdgeInsets.symmetric(
+            //               horizontal: 16, vertical: 32),
+            //           child: Column(
+            //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //             children: [
+            //               TextField(
+            //                 keyboardType: TextInputType.emailAddress,
+            //                 decoration: InputDecoration(
+            //                   labelText: 'Email',
+            //                   enabledBorder: OutlineInputBorder(
+            //                     borderSide: BorderSide(color: Colors.blue),
+            //                   ),
+            //                 ),
+            //               ),
+            //               TextField(
+            //                 obscureText: true,
+            //                 decoration: InputDecoration(
+            //                   labelText: 'Senha',
+            //                   border: OutlineInputBorder(),
+            //                 ),
+            //               ),
+            //             ],
+            //           ),
+            //         ),
+            //       ),
+            //     )
+            //   ],
+            // ),
           ],
         ),
       ),
