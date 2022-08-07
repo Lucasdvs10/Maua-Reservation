a1c608602085784774e95f3ea26c65aa83745ed3
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-08-07 13:12:07 -0300

    Deletei pasta inutil

--- maua_reservation/android/app/src/main/kotlin/com/example/maua_reservation/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.maua_reservation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- maua_reservation/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:maua_reservation/reservation_app.dart';
-
-void main() {
-  runApp(const ReservationApp());
-}
